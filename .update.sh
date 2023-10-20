e51b28a1cdba865923fddcb621a292f9280db82f
Brian Leonard <brianleonard@Brians-MacBook-Pro.local>
2023-10-20 12:07:48 -0400

    updated schema model

---
+++ .update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 schema_model/tools/convert_to_json.py schema_model/versions/1.0.0
+mv schema.json schema_model/versions/jsons/1.0.0 
+cp schema_model/versions/jsons/1.0.0/schema.json schema_model/versions/jsons/latest 
+git add .   
+git commit -m "updated schema model"
+git push origin develop 
